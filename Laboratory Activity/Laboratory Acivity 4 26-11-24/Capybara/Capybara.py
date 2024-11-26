1ffc614b130626e1bd213f76b3adcc4a505b958c
fedelhynb <ms.fedelhynb@gmail.com>
2024-11-26 18:36:55 +0800

    laboratory act

--- Laboratory Activity/Laboratory Acivity 4 26-11-24/Capybara/Capybara.py
+++ Laboratory Activity/Laboratory Acivity 4 26-11-24/Capybara/Capybara.py
@@ -7,4 +7,4 @@ class Capybara:
         self.age = age
 
     def __str__(self):
-        return f"Name: {self.name}, Gender: {self.gender}, Age: {self.age}years old"
+        return f"Name: {self.name}, Gender: {self.gender}, Age: {self.age} years old"
